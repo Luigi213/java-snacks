b7a8c5b31db564feff6ef0f28aafc68cac778e01
Luigi213 <reyesgianluigi88@gmail.com>
2023-05-05 12:50:34 +0200

    optimize snaks 2

--- src/org/snaks/Snaks3.java
+++ src/org/snaks/Snaks3.java
@@ -4,10 +4,8 @@ public class Snaks3 {
 	public static void main(String[] args) {
 		int[] arrNum = {5, 3, 4, 5};
 		int sum = 0;
-		for (int i=0;i<arrNum.length; i++) {
-			if(!(i % 2 == 0)) {
-				sum += arrNum[i - 1];
-			}
+		for (int i=0;i<arrNum.length; i+=2) {
+			sum += arrNum[i];
 		}
 		System.out.println(sum);
 	}
