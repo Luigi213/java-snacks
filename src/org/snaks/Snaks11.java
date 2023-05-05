3fde92a059fde6f00d522a2fff3e3f0327cf61fb
Luigi213 <reyesgianluigi88@gmail.com>
2023-05-05 16:58:40 +0200

    snaks 11 done

---
+++ src/org/snaks/Snaks11.java
@@ -0,0 +1,20 @@
+package org.snaks;
+
+import java.util.Scanner;
+
+public class Snaks11 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Inserire un valore numerico");
+		int num = in.nextInt();
+		
+		in.close();
+		
+		for(int i=1 ; i<num; i++) {
+			if(num % i == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
