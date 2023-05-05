4fe5909d0d6604797cfa321b4a330b32828b3135
Luigi213 <reyesgianluigi88@gmail.com>
2023-05-05 14:44:04 +0200

    snaks 7 done

---
+++ src/org/snaks/Snaks7.java
@@ -0,0 +1,17 @@
+package org.snaks;
+
+import java.util.Random;
+
+public class Snaks7 {
+	public static void main(String[] args) {
+		
+		Random rand = new Random();
+		
+		int i = 1;
+		while(i % 3 != 0 && i % 5 != 0) {			
+			i = rand.nextInt(100);
+		}
+		System.out.println(i);
+		
+	}
+}
